bf6fd462284b1802dde72c645a85bb319a1e4795
Sebastiano <sfranchy06@gmail.com>
2026-02-09 19:27:34 +0100

    Update web_steps.py

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -104,7 +104,11 @@ def step_impl(context, element_name):
 # to get the element id of any button
 ##################################################################
 
-## UPDATE CODE HERE ##
+@when('I press the "{button}" button')
+def step_impl(context, button):
+    button_id = f"{element_name.lower().replace(' ', '_')}
+    # Generate the button_id by converting the button name to lowercase and appending '-btn'
+    # Use context.driver.find_element_by_id(button_id) line to find the button element on the web page based on the generated button_id and call the click() method 
 
 ##################################################################
 # This code works because of the following naming convention:
