45dd87bd825383c84986d3dca3b7de46484f0d4f
Sebastiano <sfranchy06@gmail.com>
2026-02-10 11:13:13 +0100

    Update factories.py

--- tests/factories.py
+++ tests/factories.py
@@ -21,6 +21,7 @@ import factory
 from factory.fuzzy import FuzzyChoice, FuzzyDecimal
 from service.models import Product, Category
 
+
 class ProductFactory(factory.Factory):
     """Creates fake products for testing"""
 
