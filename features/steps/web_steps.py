20cdf725af49c66e916317c807873036cf80f425
Sebastiano <sfranchy06@gmail.com>
2026-02-10 10:58:40 +0100

    Update web_steps.py

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -104,11 +104,39 @@ def step_impl(context, element_name):
 # to get the element id of any button
 ##################################################################
 
+
 @when('I press the "{button}" button')
 def step_impl(context, button):
-    button_id = f"{element_name.lower().replace(' ', '_')}
-    # Generate the button_id by converting the button name to lowercase and appending '-btn'
-    # Use context.driver.find_element_by_id(button_id) line to find the button element on the web page based on the generated button_id and call the click() method 
+    button_id = f"{button.lower()}-btn"
+    context.driver.find_element_by_id(button_id).click()
+
+
+
+@then('I should see "{name}" in the results')
+def step_impl(context, name):
+    found = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'search_results'),
+            name
+        )
+    )
+    assert(found)
+
+@then(u'I should not see "{name}" in the results')
+def step_impl(context, name):
+    element = context.driver.find_element_by_id('search_results')
+    assert(name not in element.text)
+
+
+@then('I should see the message "{message}"')
+def step_impl(context, message):
+    found = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'flash_message'),
+            message
+        )
+    )
+    assert(found)
 
 ##################################################################
 # This code works because of the following naming convention:
